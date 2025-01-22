fe2048d882b5d1a5c3c10a01efcd82eb134edc51
Rezzaklalala68 <jerebtsovanton@gmail.com>
2025-01-22 13:56:03 +0300

    Сделано x2

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -17,10 +17,10 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru").setCar(new Car("BMW", 1311231)));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru").setCar(new Car("Mazda", 4413223)));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru").setCar(new Car("Opel", 143243525)));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru").setCar(new Car("Jeep", 13255323)));
+      userService.add(new User("Anton", "Lastname1", "user1@mail.ru").setCar(new Car("BMW", 1311231)));
+      userService.add(new User("Max", "Lastname2", "user2@mail.ru").setCar(new Car("Mazda", 4413223)));
+      userService.add(new User("Nikita", "Lastname3", "user3@mail.ru").setCar(new Car("Opel", 143243525)));
+      userService.add(new User("Sam", "Lastname4", "user4@mail.ru").setCar(new Car("Jeep", 13255323)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
